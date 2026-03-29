7a219fdd5f6c4ecf7b1244cfac9a6d6da64dc958
Smith Junior Prud'homme <sjprudhome@joinhep.org>
2026-03-29 18:03:27 +0800

    all

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      injectRegister: 'auto',
-      workbox: {
-        globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
-        cleanupOutdatedCaches: true,
-      }
-    })
-  ],
+  plugins: [react()],
   server: {
     host: true,
     port: 5173
