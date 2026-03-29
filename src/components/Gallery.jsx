51c2da61bd1c3610529b034a4b61a4bf94600c1c
Smith Junior Prud'homme <sjprudhome@joinhep.org>
2026-03-29 23:22:11 +0800

    all

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -5,9 +5,12 @@ const photos = [
   'https://i.postimg.cc/sDbrhR60/jimeng-2026-03-29-3383-make-this-picture-more-clear-and-higher.png',
   'https://i.postimg.cc/8cyqL4xQ/b4d3697f32db8.png',
   'https://i.postimg.cc/G3NKT651/ca8e3d0da04c9.png',
-  'https://i.postimg.cc/sDbrhR60/jimeng-2026-03-29-3383-make-this-picture-more-clear-and-higher.png',
-  'https://i.postimg.cc/sDbrhR60/jimeng-2026-03-29-3383-make-this-picture-more-clear-and-higher.png',
-  'https://i.postimg.cc/sDbrhR60/jimeng-2026-03-29-3383-make-this-picture-more-clear-and-higher.png',
+  'https://i.postimg.cc/bwMSmj1D/16469749dce31.png',
+  'https://i.postimg.cc/d1JsBWNw/fa9fe0865cf9a.png',
+  'https://i.postimg.cc/cL7Pmdpm/fccff0abbc93e.png',
+  'https://i.postimg.cc/W1DwN8nv/569ea474542e8.png',
+  'https://i.postimg.cc/KjPvr6xB/2335b54f51a058.png',
+  'https://i.postimg.cc/W3cB6h3y/8d096ebb6ebf38.png'
 ]
 
 export default function Gallery() {
