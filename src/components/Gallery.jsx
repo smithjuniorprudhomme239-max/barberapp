bff131684e49d700894d7ade264089c2582e78c1
Smith Junior Prud'homme <sjprudhome@joinhep.org>
2026-03-29 17:44:59 +0800

    all

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -2,12 +2,12 @@ import { useState, useEffect } from 'react'
 import './Gallery.css'
 
 const photos = [
-  'https://images.unsplash.com/photo-1621605815971-fbc98d665033?w=600',
-  'https://images.unsplash.com/photo-1599351431202-1e0f0137899a?w=600',
-  'https://images.unsplash.com/photo-1622286342621-4bd786c2447c?w=600',
-  'https://images.unsplash.com/photo-1503951914875-452162b0f3f1?w=600',
-  'https://images.unsplash.com/photo-1585747860715-2ba37e788b70?w=600',
-  'https://images.unsplash.com/photo-1512690459411-b9245aed614b?w=600',
+  'https://i.postimg.cc/sDbrhR60/jimeng-2026-03-29-3383-make-this-picture-more-clear-and-higher.png',
+  'https://i.postimg.cc/sDbrhR60/jimeng-2026-03-29-3383-make-this-picture-more-clear-and-higher.png',
+  'https://i.postimg.cc/sDbrhR60/jimeng-2026-03-29-3383-make-this-picture-more-clear-and-higher.png',
+  'https://i.postimg.cc/sDbrhR60/jimeng-2026-03-29-3383-make-this-picture-more-clear-and-higher.png',
+  'https://i.postimg.cc/sDbrhR60/jimeng-2026-03-29-3383-make-this-picture-more-clear-and-higher.png',
+  'https://i.postimg.cc/sDbrhR60/jimeng-2026-03-29-3383-make-this-picture-more-clear-and-higher.png',
 ]
 
 export default function Gallery() {
--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      injectRegister: 'auto',
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
+        cleanupOutdatedCaches: true,
+      }
+    })
+  ],
   server: {
     host: true,
     port: 5173
